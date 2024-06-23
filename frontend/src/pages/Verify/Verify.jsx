2febfc9a4ce4fb19afeddf01b783c9fbd7cb58b6
Anant Saxena <anantsaxena5454@gmail.com>
2024-06-23 22:24:34 +0530

    Deployment testing by changing localhost by the deployment link

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
 // import React from 'react'
 import { useSearchParams } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom';
